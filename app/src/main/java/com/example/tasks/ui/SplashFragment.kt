f92f4a76f018f0eafe759c4215b7ac225b70572f
rafaelsouza <rafael.mycontact@gmail.com>
2023-08-07 15:16:50 -0300

    created initial app navigation

--- app/src/main/java/com/example/tasks/ui/SplashFragment.kt
+++ app/src/main/java/com/example/tasks/ui/SplashFragment.kt
@@ -1,10 +1,14 @@
 package com.example.tasks.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.tasks.R
 import com.example.tasks.databinding.FragmentSplashBinding
 
 
@@ -21,6 +25,14 @@ class SplashFragment : Fragment() {
     ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 2000)
+    }
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/tasks/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/tasks/ui/auth/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.tasks.R
 import com.example.tasks.databinding.FragmentLoginBinding
 import com.example.tasks.databinding.FragmentRegisterBinding
@@ -21,6 +23,19 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+    private fun initListener(){
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.btnRecoverAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
     }
 
     override fun onDestroyView() {
